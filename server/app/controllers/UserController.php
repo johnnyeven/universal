92f4cff05fc34f1dfa5c24e53b077743d74619fc
johnnyeven_work <johnnyeven@gmail.com>
2014-06-06 10:36:57 +0800

    1

---
+++ server/app/controllers/UserController.php
@@ -0,0 +1,117 @@
+<?php
+
+class UserController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+	const USER_LOGIN_SUCCESS = 1;
+	const USER_LOGIN_ERROR_AUTH_FAIL = 1008;
+	const USER_LOGIN_ERROR_NO_PARAM = 1009;
+
+	const USER_REGISTER_SUCCESS = 1;
+	const USER_REGISTER_ERROR_DUPLICATED = 2007;
+	const USER_REGISTER_ERROR_AUTH_FAIL = 2008;
+	const USER_REGISTER_ERROR_NO_PARAM = 2009;
+
+	const USER_SYNC_SUCCESS = 1;
+	const USER_SYNC_ERROR_NOT_EXIST = 3007;
+	const USER_SYNC_ERROR_NO_CHANGE = 3008;
+	const USER_SYNC_ERROR_NO_PARAM = 3009;
+	
+	public function login()
+	{
+		$username = Input::get('username');
+		$password = Input::get('password');
+
+		if(!empty($username) && !empty($password))
+		{
+			if(Auth::attempt(array(
+				'name'		=>	$username,
+				'pass'		=>	$password
+			)))
+			{
+				$user = Auth::user();
+				$user->lasttime = time();
+				$user->save();
+
+				return Response::json(array(
+					'success'	=>	1,
+					'code'		=>	UserController::USER_LOGIN_SUCCESS,
+					'user'		=>	$user
+				));
+			}
+			else
+			{
+				return Response::json(array(
+					'success'	=>	0,
+					'code'		=>	UserController::USER_LOGIN_ERROR_AUTH_FAIL
+				));
+			}
+		}
+		else
+		{
+			return Response::json(array(
+				'success'	=>	0,
+				'code'		=>	UserController::USER_LOGIN_ERROR_NO_PARAM
+			));
+		}
+	}
+
+	public function register()
+	{
+		$username = Input::get('username');
+		$password = Input::get('password');
+
+		if(!empty($username) && !empty($password))
+		{
+			$userCount = User::where('name', '=', $username)->count();
+			if(empty($userCount))
+			{
+				$time = time();
+				$user = new User;
+				$user->name = $username;
+				$user->pass = Hash::make($password);
+				$user->email = '';
+				$user->lasttime = $time;
+				$user->regtime = $time;
+				$user->save();
+
+				return Response::json(array(
+					'success'	=>	1,
+					'code'		=>	UserController::USER_REGISTER_SUCCESS,
+					'user'		=>	$user
+				));
+			}
+			else
+			{
+				return Response::json(array(
+					'success'	=>	0,
+					'code'		=>	UserController::USER_REGISTER_ERROR_DUPLICATED
+				));
+			}
+		}
+		else
+		{
+			return Response::json(array(
+				'success'	=>	0,
+				'code'		=>	UserController::USER_REGISTER_ERROR_NO_PARAM
+			));
+		}
+	}
+
+	public function start()
+	{
+		
+	}
+
+}
